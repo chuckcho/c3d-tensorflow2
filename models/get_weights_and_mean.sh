a84156e95cd857f831aa7cbc8a418739c198f9f5
Chuck Cho <chuck.cho@dextro.co>
2016-11-30 13:05:08 -0500

    Put downloaded files into right place

--- models/get_weights_and_mean.sh
+++ models/get_weights_and_mean.sh
@@ -1,2 +1,20 @@
-wget -nc http://vlg.cs.dartmouth.edu/c3d/conv3d_deepnetA_sport1m_iter_1900000
-wget -nc https://github.com/facebook/C3D/raw/master/examples/c3d_finetuning/train01_16_128_171_mean.binaryproto
+#!/usr/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+echo ---------------------------------------------
+echo Downloading Sports1mil pre-trained model...
+wget \
+  -nc \
+  http://vlg.cs.dartmouth.edu/c3d/conv3d_deepnetA_sport1m_iter_1900000 \
+  --directory-prefix=${DIR}
+
+echo ---------------------------------------------
+echo Downloading mean cube
+wget \
+  -nc \
+  https://github.com/facebook/C3D/raw/master/examples/c3d_finetuning/train01_16_128_171_mean.binaryproto \
+  --directory-prefix=${DIR}
+
+echo ---------------------------------------------
+echo Done!
